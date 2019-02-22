12c0ad26ac0e6325caefa6d8878bd6c8df948d79
Eric <elymberopoulos@luc.edu>
2019-02-22 16:25:01 -0600

    index file format fixed. wrapped index in try catch. deleted bulb.java??

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 
 var readlineSync = require('readline-sync');
+const dgram = require('dgram');
+var s = dgram.createSocket('udp4');
 
 const rl = readlineSync.createInterface;
 
-start();
-                        
-function start(){
 
-          while(true)
-          {
-              var ans = readlineSync.question("What would you like to do? Type help for options");
-              switch(ans) {
-                          case "add device":
-                              addDevice()
-                            break;
-                          case "remove device":
-                              removeDevice()
-                            break;
-                          case "manage device":
-                              sayHello()
-                            break;
-                          case "move device":
-                              moveDevice();
-                            break;
-                          case "show device with state":
-                              showDeviceswstate()
-                            break;
-                          case "show device":
-                              showDevices()
-                            break;
-                          default:
-                            console.log("Invalid Input")
-                        }
-                    
-                  };    
-                }
-        
-        
-          
-          
+function start() {
+try {
+  while (true) {
+    var ans = readlineSync.question("What would you like to do? Type help for options");
+    switch (ans) {
+      case "add device":
+        addDevice()
+        break;
+      case "remove device":
+        removeDevice()
+        break;
+      case "manage device":
+        sayHello()
+        break;
+      case "move device":
+        moveDevice();
+        break;
+      case "show device with state":
+        showDeviceswstate()
+        break;
+      case "show device":
+        showDevices()
+        break;
+      case "test send":
+        s.send(Buffer.from('abc'), 3000, 'localhost');
+        s.on('message', function(msg, rinfo) {
+          console.log('I got this message: ' + msg.toString());
+        });
+        s.bind(3000);
+        break;
+      default:
+        console.log("Invalid Input")
+    }
+  };
+} catch (error) {
+  
+}
+
+}
+start();
+
+
+
+
+
