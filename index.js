1952f16ae5ea03f2e6c7dcbb9725acd47c71541c
ivankavanagh <ikavanagh@luc.edu>
2019-02-22 18:09:31 -0600

    pushing experimental server

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-
-var readlineSync = require('readline-sync');
+const readlineSync = require("readline-sync");
+const powerStrips = require("./src/devices/Device");
+const smartLight = require("./src/devices/PowerStrip");
+const powerStrip = require("./src/devices/SmartLight");
+const myDevice = require("./src/devices/Device");
 
 const rl = readlineSync.createInterface;
 
-start();
-                        
-function start(){
 
-          while(true)
-          {
-              var ans = readlineSync.question("What would you like to do? Type help for options");
-              switch(ans) {
-                          case "add device":
-                              addDevice()
-                            break;
-                          case "remove device":
-                              removeDevice()
-                            break;
-                          case "manage device":
-                              sayHello()
-                            break;
-                          case "move device":
-                              moveDevice();
-                            break;
-                          case "show device with state":
-                              showDeviceswstate()
-                            break;
-                          case "show device":
-                              showDevices()
-                            break;
-                          default:
-                            console.log("Invalid Input")
-                        }
-                    
-                  };    
-                }
-        
-        
-          
-          
+
+function start() {
+  while (true) {
+    var ans = readlineSync.question(
+      "What would you like to do? Type help for options"
+    );
+    switch (ans) {
+      case "add device":
+        var device = new myDevice.Device(false);
+        break;
+      case "remove device":
+        removeDevice();
+        break;
+      case "manage device":
+        sayHello();
+        break;
+      case "move device":
+        moveDevice();
+        break;
+      case "show device with state":
+        showDeviceswstate();
+        break;
+      case "show device":
+        showDevices();
+        break;
+      default:
+        console.log("Invalid Input");
+    }
+  }
+} start();
+
+
--- src/devices/Device.js
+++ src/devices/Device.js
@@ -1,10 +1,18 @@
 const timer = require('./timer/Timer');
+const echo = require('./client');
+var client = new echo.client; 
 
 class Device{
-    constructor(){   
-        this.power = false;
+    constructor(power){   
+
+        this.power = power;
         this.timer = new timer.Timer(this);
+        client = new echo.client; 
+        client.sendData(power.toString());
+        
+        
     }
+
 
 }
 module.exports.Device = Device;
