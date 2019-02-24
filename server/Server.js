3c589e0c8fdc234218f112537251cd7826a650e5
Eric <elymberopoulos@luc.edu>
2019-02-24 00:41:14 -0600

    Code restructure and work on server

--- server/Server.js
+++ server/Server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const fileSys = require('fs');
 const dgram = require('dgram');
 const customStorage = require('./storageInit/Storage');
-
 const port = process.env.PORT || 3000;
 const socket = dgram.createSocket('udp4');
 
@@ -15,33 +14,20 @@ const storageRoot = customStorage.storageDir;
 const powerStripDir = customStorage.powerStripDir;
 const lightsDir = customStorage.lightDir;
 
-
 /////////////////////////////////////////
 
+// const server = express();
 
-const server = express();
+socket.on('message', function (msg, rinfo) {
+    console.log('I got this message: ' + msg.toString());
+    console.log('message address: ' + rinfo.address);
+    console.log('message port: ' + rinfo.port);
 
-socket.bind(port);
-
-server.listen(port), async err => {
-    if (err) {
-        console.log("Error: " + err);
-    }
-}
-
-server.on("message", function (msg, rinfo) {
-    console.log(rinfo.address + ':' + rinfo.port + ' - ' + msg);
-    server.send(msg, 0, msg.length, rinfo.port, rinfo.address);
 });
-
-// socket.on('message', function (msg, rinfo) {
-//     console.log('I got this message: ' + msg.toString());
-//     console.log('message address: ' + rinfo.address);
-//     console.log('message port: ' + rinfo.port);
-
-// });
 
 socket.on('listening', () => {
     const address = socket.address();
-    console.log(`server listening ${address.address}:${address.port}`);
+    console.log(`socket server listening ${address.address}:${address.port}`);
 });
+
+socket.bind(3000);
